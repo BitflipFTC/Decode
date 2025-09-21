49b4c69b7dae2d04550585f6e1840e0ce639c17c
UnheardWolf344 <griffin3868@gmail.com>
2025-09-21 16:42:59 -0400

    fix loop timer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/LoopTimer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/LoopTimer.kt
@@ -4,13 +4,14 @@ import com.qualcomm.robotcore.util.ElapsedTime
 
 class LoopTimer() {
     private val timer : ElapsedTime = ElapsedTime()
-    private var lastTime : Double = timer.milliseconds()
 
     fun getLoopTime() : Double {
-        return timer.milliseconds() - lastTime
+        val x = timer.milliseconds()
+        reset()
+        return x
     }
 
     fun reset() {
-        lastTime = timer.milliseconds()
+        timer.reset()
     }
 }
