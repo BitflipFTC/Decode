f5cab0eb6349977747ccc903c094511bbc26f6bf
UnheardWolf344 <griffin3868@gmail.com>
2025-10-05 23:44:22 -0400

    Make turret servos use full range

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Turret.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Turret.kt
@@ -1,11 +1,19 @@
 package org.firstinspires.ftc.teamcode.util
 
 import com.qualcomm.robotcore.hardware.CRServo
+import com.qualcomm.robotcore.hardware.CRServoImplEx
 import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.PwmControl
 
 class Turret(val hwMap : HardwareMap) {
-    val servoL : CRServo by lazy { hwMap["turretR"] as CRServo }
-    val servoR : CRServo by lazy { hwMap["turretL"] as CRServo }
+    val servoL : CRServoImplEx by lazy { hwMap["turretR"] as CRServoImplEx }
+    val servoR : CRServoImplEx by lazy { hwMap["turretL"] as CRServoImplEx }
+
+    init {
+        // defaults to 600,2400 - this gives full range
+        servoR.pwmRange = PwmControl.PwmRange(500.0, 2500.0)
+        servoL.pwmRange = PwmControl.PwmRange(500.0, 2500.0)
+    }
 
     fun setPower (pow : Double) {
         servoR.power = pow
