54495712b80f23f4c1e11c3a8d00510664f5926e
UnheardWolf344 <griffin3868@gmail.com>
2025-10-24 17:51:49 -0400

    cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TurretLoggerTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/TurretLoggerTest.kt
@@ -10,7 +10,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
-import com.qualcomm.robotcore.hardware.Gamepad.LED_DURATION_CONTINUOUS
 import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.util.ElapsedTime
 import gay.zharel.fateweaver.flight.FlightRecorder
