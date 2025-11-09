0267e49fa0c7f4528ae80f62ae026ad6e2eddb0f
UnheardWolf344 <griffin3868@gmail.com>
2025-11-09 12:00:39 -0500

    add led feedback, fix logs, add color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FullCommandOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FullCommandOpMode.kt
@@ -5,6 +5,9 @@ import com.acmerobotics.dashboard.FtcDashboard
 import com.bylazar.telemetry.JoinedTelemetry
 import com.bylazar.telemetry.PanelsTelemetry
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Gamepad
+import dev.nextftc.bindings.button
+import dev.nextftc.core.commands.delays.WaitUntil
 import dev.nextftc.core.commands.groups.ParallelGroup
 import dev.nextftc.core.commands.groups.SequentialGroup
 import dev.nextftc.core.commands.utility.InstantCommand
@@ -14,6 +17,7 @@ import dev.nextftc.core.components.SubsystemComponent
 import dev.nextftc.ftc.Gamepads
 import dev.nextftc.ftc.components.BulkReadComponent
 import dev.nextftc.ftc.components.LoopTimeComponent
+import org.firstinspires.ftc.teamcode.subsystems.ArtifactColorSensor
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain
 import org.firstinspires.ftc.teamcode.subsystems.Intake
 import org.firstinspires.ftc.teamcode.subsystems.OV9281
@@ -39,6 +43,7 @@ class FullCommandOpMode: BitflipOpMode() {
     val spindexer = Spindexer()
     val transfer = Transfer()
     val turret = Turret()
+    val colorSensor = ArtifactColorSensor()
 
     var alliance = Alliance.NONE
 
@@ -57,31 +62,23 @@ class FullCommandOpMode: BitflipOpMode() {
                 shooter,
                 spindexer,
                 transfer,
-                turret
+                turret,
+                colorSensor
             ),
             BulkReadComponent,
             BindingsComponent,
             LoopTimeComponent(),
-            TelemetryComponent // updates telemetry during waitforstart and update
         )
     }
 
     override fun onInit() {
         telemetry = JoinedTelemetry(telemetry, PanelsTelemetry.ftcTelemetry, FtcDashboard.getInstance().telemetry)
-        telemetry.msTransmissionInterval = 500
+//        telemetry.msTransmissionInterval = 250
 
         drivetrain.fieldCentric = true
+        shooter.hoodPosition = 0.0
+        shooter.periodic()
     }
-
-    /*
-    enum class InitializeState {
-        GET_MOTIF,
-    }
-
-    var initializeState = InitializeState.GET_MOTIF
-    var selectedMotif = MotifPattern.NONE
-    val allMotifs = MotifPattern.entries.toTypedArray()
-    */
 
     override fun onWaitForStart() {
         telemetry.addData("Subsystems", "initialized")
@@ -98,6 +95,7 @@ class FullCommandOpMode: BitflipOpMode() {
         } else {
             alliance
         }
+
     /*
     when (initializeState) {
             InitializeState.GET_MOTIF -> {
@@ -160,14 +158,15 @@ class FullCommandOpMode: BitflipOpMode() {
 
         Gamepads.gamepad1.triangle.whenBecomesTrue(
             SequentialGroup(
-                InstantCommand { Log.d("COMMAND_TIMER", "Start time: ${System.nanoTime() / 1000000}ms")},
+                InstantCommand { Log.d("COMMAND_TIMER", "Start time: ${System.nanoTime() / 1000000}")},
                 RepeatCommand(
                     SequentialGroup(
                         spindexer.tryMotifOuttake(),
+                        WaitUntil(shooter::atSetPoint),
                         retryShoot().thenWait(200.milliseconds)
                     ), spindexer::totalFullSlots
                 ).then(spindexer.goToFirstEmptyIntake()),
-                InstantCommand { Log.d("COMMAND_TIMER", "End time: ${System.nanoTime() / 1000000}ms")}
+                InstantCommand { Log.d("COMMAND_TIMER", "End time: ${System.nanoTime() / 1000000}")}
             )
         )
 
@@ -199,6 +198,11 @@ class FullCommandOpMode: BitflipOpMode() {
         Gamepads.gamepad1.cross  whenBecomesTrue intake.reverse()   whenBecomesFalse intake.forward()
 
         Gamepads.gamepad1.circle whenBecomesTrue InstantCommand { drivetrain.resetYaw() }
+
+        // gamepad red when no atag seen
+        button {camera.distanceToGoal > 0.0} whenBecomesTrue InstantCommand {gamepad1.setLedColor(0.0,255.0,0.0,
+            Gamepad.LED_DURATION_CONTINUOUS)} whenBecomesFalse InstantCommand {gamepad1.setLedColor(255.0,0.0,0.0,
+            Gamepad.LED_DURATION_CONTINUOUS)}
     }
 
 }
