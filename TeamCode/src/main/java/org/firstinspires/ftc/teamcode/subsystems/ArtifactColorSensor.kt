46dbbdd8e72eb923443d8a00255b5b3e2a62fa0a
UnheardWolf344 <griffin3868@gmail.com>
2025-11-09 11:58:21 -0500

    Add color sensor class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ArtifactColorSensor.kt
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.subsystems
+
+import com.qualcomm.robotcore.hardware.ColorRangeSensor
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor
+import com.qualcomm.robotcore.hardware.NormalizedRGBA
+import dev.nextftc.core.subsystems.Subsystem
+import dev.nextftc.ftc.ActiveOpMode
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.teamcode.util.Artifact
+
+class ArtifactColorSensor: Subsystem {
+    private lateinit var colorSensor: ColorRangeSensor
+    var colors: NormalizedRGBA = NormalizedRGBA()
+        private set
+
+    var detectedArtifact = Artifact.NONE
+
+    override fun initialize() {
+        colorSensor = ActiveOpMode.hardwareMap.get(ColorRangeSensor::class.java, "colorSensor")
+    }
+
+    override fun periodic() {
+        colors = colorSensor.normalizedColors
+        detectedArtifact = if (colors.red / colors.alpha <= 0.02 && colors.green / colors.alpha <= 0.02 && colors.blue / colors.alpha >= 0.023) {
+            Artifact.PURPLE
+        } else if (colors.red / colors.alpha <= 0.02 && colors.green / colors.alpha >= 0.023 && colors.blue / colors.alpha <= 0.022) {
+            Artifact.GREEN
+        } else {
+            Artifact.NONE
+        }
+
+        ActiveOpMode.telemetry.run {
+            addData("Normalized R", colors.red / colors.alpha)
+            addData("Normalized G", colors.green / colors.alpha)
+            addData("Normalized B", colors.blue / colors.alpha)
+            addData("R", colors.red)
+            addData("G", colors.green)
+            addData("B", colors.blue)
+            addLine()
+            addData("Detected Artifact", detectedArtifact.name)
+            addData("Light detected", colorSensor.lightDetected)
+            addData("Distance", "%05.2fcm", colorSensor.getDistance(DistanceUnit.CM))
+            addLine("------------------------------")
+        }
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/utility/ColorSensorTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/utility/ColorSensorTest.kt
@@ -5,11 +5,15 @@ import com.bylazar.telemetry.JoinedTelemetry
 import com.bylazar.telemetry.PanelsTelemetry
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.ColorRangeSensor
 import com.qualcomm.robotcore.hardware.ColorSensor
+import com.qualcomm.robotcore.hardware.DistanceSensor
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.teamcode.subsystems.Intake
 
 @TeleOp(name = "Test: Color Sensor", group = "Test")
 class ColorSensorTest : LinearOpMode() {
-    val colorSensor by lazy { hardwareMap["color"] as ColorSensor }
+    val colorSensor by lazy { hardwareMap["colorSensor"] as ColorRangeSensor }
     override fun runOpMode() {
         telemetry = JoinedTelemetry(PanelsTelemetry.ftcTelemetry, telemetry)
         waitForStart()
@@ -18,10 +22,13 @@ class ColorSensorTest : LinearOpMode() {
             telemetry.addData("Green", colorSensor.green())
             telemetry.addData("Blue", colorSensor.blue())
             telemetry.addData("Alpha", colorSensor.alpha())
-            telemetry.addData("ARGB", colorSensor.argb())
+            val colors = colorSensor.normalizedColors
+            telemetry.addData("i2c address", colorSensor.i2cAddress)
+            telemetry.addData("Distance", "%05.2fcm", colorSensor.getDistance(DistanceUnit.CM))
 
-            telemetry.addData("r", Color.red(colorSensor.argb()))
-            telemetry.addData("g", Color.blue(colorSensor.argb()))
+            telemetry.addLine()
+            telemetry.addData("r", colors.red / colors.alpha)
+            telemetry.addData("g", colors.green / colors.alpha)
             telemetry.addData("b", Color.green(colorSensor.argb()))
 
             telemetry.update()
