97ac649405e94cf5d07f5c0c77eabad0f988c4c2
UnheardWolf344 <griffin3868@gmail.com>
2025-12-03 01:22:24 -0500

    fix: Update combined teleop to hopefully worrk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CombinedTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CombinedTeleOp.kt
@@ -3,12 +3,14 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop
 import com.bylazar.configurables.annotations.Configurable
 import com.bylazar.telemetry.JoinedTelemetry
 import com.bylazar.telemetry.PanelsTelemetry
+import com.bylazar.utils.LoopTimer
 import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.teamcode.subsystems.ArtifactColorSensor
 import org.firstinspires.ftc.teamcode.subsystems.Shooter
 import org.firstinspires.ftc.teamcode.subsystems.Spindexer
 import org.firstinspires.ftc.teamcode.subsystems.Transfer
@@ -17,28 +19,13 @@ import org.firstinspires.ftc.teamcode.subsystems.Drivetrain
 import org.firstinspires.ftc.teamcode.subsystems.Intake
 import org.firstinspires.ftc.teamcode.subsystems.OV9281
 import org.firstinspires.ftc.teamcode.util.Alliance
+import org.firstinspires.ftc.teamcode.util.InitConfigurer
 import org.firstinspires.ftc.teamcode.util.dpadToAxes
 import kotlin.math.abs
 
 //@Configurable
 //@TeleOp(name = "Combined TeleOp", group = "TeleOp")
 class CombinedTeleOp : LinearOpMode() {
-    companion object {
-        @JvmField
-        var flywheelRPM = 0.0
-        @JvmField
-        var hoodPosition = 0.2
-    }
-    // Drivetrain
-    private var fieldCentric : Boolean = false
-    private val driveSpeed = 0.8
-    lateinit var camera: OV9281
-
-    val targetTagBearing = 0.0
-    // for dashboard purposes
-
-    var alliance: Alliance = Alliance.NONE
-
     // shooter logic is as follows
     // when right trigger is held on gp1, if there is an apriltag visible
     // it auto sets rpm and hood.
@@ -65,42 +52,41 @@ class CombinedTeleOp : LinearOpMode() {
     override fun runOpMode() {
         telemetry = JoinedTelemetry(PanelsTelemetry.ftcTelemetry, telemetry)
         telemetry.setDisplayFormat(Telemetry.DisplayFormat.HTML)
-        val drivetrain = Drivetrain
+        val drivetrain = Drivetrain.apply {
+            fieldCentric = false
+        }
         val intake = Intake
         val transfer = Transfer()
         val spindexer = Spindexer
         val shooter = Shooter
         val turret = Turret
+        val camera = OV9281()
+        val colorSensor = ArtifactColorSensor
+        val subsystems = listOf(
+            drivetrain,
+            intake,
+            transfer,
+            spindexer,
+            shooter,
+            turret,
+            camera,
+            colorSensor
+        )
+        val loopTimer = LoopTimer(10)
 
-        camera = OV9281()
+        subsystems.forEach { it.initialize() }
 
+        InitConfigurer.preInit()
         while (opModeInInit()) {
-            telemetry.addData("Drivetrain", "initialized")
-            telemetry.addLine("Press CIRCLE for Red  alliance")
-            telemetry.addLine("Press CROSS  for Blue alliance")
-            telemetry.addLine()
-            telemetry.addData("Selected alliance", "<strong>%s</strong>", alliance.name)
-            telemetry.addData("Selected goal", alliance.aprilTagID)
-
-            alliance = if (gamepad1.crossWasPressed()) {
-                Alliance.BLUE
-            } else if (gamepad1.circleWasPressed()) {
-                Alliance.RED
-            } else {
-                alliance
-            }
-
-            telemetry.update()
+            InitConfigurer.postWaitForStart()
         }
 
         // bulk caching
         val allHubs = hardwareMap.getAll(LynxModule::class.java)
         allHubs.forEach { hub -> hub.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL }
 
-        val timer= ElapsedTime()
-        intake.intake()
-
-        camera.targetID = alliance.aprilTagID
+        camera.targetID = InitConfigurer.selectedAlliance.aprilTagID
+        loopTimer.start()
 
         while (opModeIsActive()) {
             // more bulk caching
@@ -110,18 +96,12 @@ class CombinedTeleOp : LinearOpMode() {
             // for gamepad layouts
 
             // drivetrain
-            if (gamepad1.touchpadWasPressed()) fieldCentric = !fieldCentric
+            if (gamepad1.touchpadWasPressed()) Drivetrain.fieldCentric = !Drivetrain.fieldCentric
             if (gamepad1.circleWasPressed()) drivetrain.resetYaw()
             drivetrain.setDrivetrainPowers(drivetrain.calculateDrivetrainPowers(
                 gamepad1.left_stick_x.toDouble(),
                 -gamepad1.left_stick_y.toDouble(),
                 gamepad1.right_stick_x.toDouble()))
-
-            drivetrain.driveSpeed = driveSpeed
-            drivetrain.fieldCentric = fieldCentric
-
-            camera.periodic()
-
 
             // transfer
             if (gamepad1.triangleWasPressed()) { transfer.transferArtifact(); gamepad1.rumble(500) }
@@ -144,56 +124,24 @@ class CombinedTeleOp : LinearOpMode() {
             if (gamepad1.crossWasPressed()) {
                 intake.reversed = true
             }
-
             if (gamepad1.crossWasReleased()) {
                 intake.reversed = false
             }
 
-            // map autoaim behind left trigger
-            if (gamepad1.left_trigger >= 0.25) {
+            // map autoaim behind right trigger
+            if (gamepad1.right_trigger >= 0.15) {
                 turret.bearing = camera.currentTagBearing
-                turret.periodic()
-            } else {
-                turret.setPower(gamepad2.left_stick_x.toDouble() * 0.15)
             }
 
-            if (gamepad1.dpadDownWasPressed()) {
-                intake.off()
-            }
-
-            // map auto adjust behind right trigger
-            if (gamepad1.right_trigger >= 0.25) {
+            // map auto adjust behind left trigger
+            if (gamepad1.left_trigger >= 0.15) {
                 shooter.setTargetState(camera.distanceToGoal)
             } else {
-                shooter.state = Shooter.ShooterState(hoodPosition, flywheelRPM)
+                shooter.targetFlywheelRPM = 0.0
             }
-
-            // shooter stuff
-            val (dpadX, dpadY) = gamepad2.dpadToAxes()
-            flywheelRPM += dpadY * 250.0
-            hoodPosition += dpadX * 0.05
 
             // update all mechanisms
-
-            // allow gamepad2 to have manual transfer control
-            if (abs(gamepad2.right_trigger - gamepad2.left_trigger) >= 0.15) {
-                transfer.setPower((gamepad2.right_trigger - gamepad2.left_trigger) * 1.0)
-                transfer.targetPosition = transfer.currentPosition
-            } else {
-                transfer.periodic()
-            }
-
-            shooter.periodic()
-            intake.periodic()
-
-            if (abs(gamepad2.right_stick_x) >= 0.15) {
-                spindexer.power = gamepad2.right_stick_x.toDouble() * 0.75
-
-                // prevent from periodic() resetting to a preset
-                spindexer.targetAngle = spindexer.currentAngle
-            } else {
-                spindexer.periodic()
-            }
+            subsystems.forEach { it.periodic() }
 
             // if we have a detection, green; otherwise, red
             if (camera.distanceToGoal > 0.0) {
@@ -202,33 +150,8 @@ class CombinedTeleOp : LinearOpMode() {
                 gamepad1.setLedColor(255.0, 0.0, 0.0, Gamepad.LED_DURATION_CONTINUOUS)
             }
 
-            telemetry.addData("Drivetrain Powers", drivetrain.currentDrivePowers.toString())
-            telemetry.addData("Heading", drivetrain.heading)
-            telemetry.addData("Field Centric?", drivetrain.fieldCentric)
-            telemetry.addLine("-------------------------------------")
-            telemetry.addData("Intake Power", intake.power.value)
-            telemetry.addData("Intake State", intake.power)
-            telemetry.addLine("-------------------------------------")
-            telemetry.addData("Transfer Target Ticks", transfer.targetPosition)
-            telemetry.addData("Transfer Current Ticks", transfer.currentPosition)
-            telemetry.addLine("-------------------------------------")
-            telemetry.addData("Spindexer current state", spindexer.state.name)
-            telemetry.addData("Spindexer target angle", spindexer.targetAngle)
-            telemetry.addData("Spindexer current angle", spindexer.currentAngle)
-            telemetry.addLine("-------------------------------------")
-            telemetry.addData("Flywheel target RPM", shooter.targetFlywheelRPM)
-            telemetry.addData("Flywheel current RPM", shooter.flywheelRPM)
-            telemetry.addData("Hood position", shooter.hoodPosition)
-            telemetry.addData("Distance (in)", camera.distanceToGoal)
-            telemetry.addLine("-------------------------------------")
-            telemetry.addData("April tag current bearing", camera.currentTagBearing)
-            telemetry.addData("April tag target bearing", targetTagBearing)
-            telemetry.addLine("-------------------------------------")
-            telemetry.addData("Turret current", turret.bearing)
-            telemetry.addData("Turret target", 0.0)
-
-            telemetry.addData("Loop time", timer.milliseconds())
-            timer.reset()
+            telemetry.addData("Loop Hz", "%05.2f", loopTimer.hz)
+            telemetry.addData("Loop ms", "%05.2f", loopTimer.ms.toDouble())
             telemetry.update()
         }
     }
