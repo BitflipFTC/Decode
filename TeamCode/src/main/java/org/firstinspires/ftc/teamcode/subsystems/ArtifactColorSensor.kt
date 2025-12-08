2547757f17eec91b2256b3065a78a96d37a47a84
UnheardWolf344 <griffin3868@gmail.com>
2025-12-08 15:11:34 -0500

    Implement read switching on color sensor to improve loop times

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ArtifactColorSensor.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ArtifactColorSensor.kt
@@ -22,13 +22,25 @@ class ArtifactColorSensor(): Subsystem {
     val blue: Double
         get() = colors.blue.toDouble()
 
+    // reading the distance is one i2c read,
+    // reading the colors is another.
+    // We split these reads into two loops, only reading one per loop,
+    // which decreases loop times.
+    // when this is false, the colors are read.
+    private var readDistance = true
+
     override fun initialize() {
         colorSensor = ActiveOpMode.hardwareMap.get(RevColorSensorV3::class.java, "colorSensor")
     }
 
     override fun periodic() {
-        colors = colorSensor.normalizedColors
-        distance = colorSensor.getDistance(DistanceUnit.CM)
+        // alternate reads to improve loop times
+        if (!readDistance) {
+            colors = colorSensor.normalizedColors
+        } else {
+            distance = colorSensor.getDistance(DistanceUnit.CM)
+        }
+        readDistance = !readDistance
 
         detectedArtifact = if (distance <= 5.0) {
             if (blue > green) {
