e49e6741ed10069d506e8f6a184cf420860cd303
UnheardWolf344 <griffin3868@gmail.com>
2026-02-01 04:25:57 -0500

    Improve auto  poses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/auto/AutoPoses.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/auto/AutoPoses.kt
@@ -10,8 +10,8 @@ class AutoPoses(
     private fun p (p: Pose): Pose = if (alliance == Alliance.RED) p else p.mirror()
 
     private val HORIZONTAL_INTAKE_START = 100.0
-    private val HORIZONTAL_INTAKE_END = 132.0
-    private val HORIZONTAL_INTAKE_MOVE_BACK = 125.0
+    private val HORIZONTAL_INTAKE_END = 134.0
+    private val HORIZONTAL_INTAKE_MOVE_BACK = 110.0
 
     private val INTAKE_1_VERTICAL = 84.0
     private val INTAKE_2_VERTICAL = 58.5
@@ -22,14 +22,14 @@ class AutoPoses(
     val farShootPoseFacingObelisk = p(Pose(88.0, 14.0, Math.toRadians(90.0))) 
     val farShootPose   = p(Pose(88.0, 14.0, Math.toRadians(0.0)))
     val farParkPose    = p(Pose(105.500, 33.500, Math.toRadians(90.0)))
-    val nearStartPose  = p(Pose(117.4, 128.9, 0.7188))
-    val nearShootPoseFacingObelisk = p(Pose(110.0, 110.0, atan2(144.0 - 110.0, 72.0 - 110.0)))
-    val nearShootPose  = p(Pose(110.0, 110.0, INTAKE_HEADING))
-    val nearParkPose   = p(Pose(125.0, 90.0, INTAKE_HEADING))
+    val nearStartPose  = p(Pose(116.4, 129.6, 0.7463))
+    val nearShootPoseFacingObelisk = p(Pose(HORIZONTAL_INTAKE_START, HORIZONTAL_INTAKE_START, atan2(144.0 - HORIZONTAL_INTAKE_START, 72.0 - HORIZONTAL_INTAKE_START)))
+    val nearShootPose  = p(Pose(HORIZONTAL_INTAKE_START, HORIZONTAL_INTAKE_START, INTAKE_HEADING))
+    val nearParkPose   = p(Pose(123.0, 90.0, INTAKE_HEADING))
     val nearIntake1Control = p(Pose(HORIZONTAL_INTAKE_START, nearShootPose.y))
     val farIntake1Control = p(Pose(farStartPose.x, INTAKE_1_VERTICAL))
     val startIntake1   = p(Pose(HORIZONTAL_INTAKE_START, INTAKE_1_VERTICAL, INTAKE_HEADING))
-    val endIntake1     = p(Pose(129.0, INTAKE_1_VERTICAL, INTAKE_HEADING))
+    val endIntake1     = p(Pose(128.0, INTAKE_1_VERTICAL, INTAKE_HEADING))
     val nearIntake2Control = p(Pose(HORIZONTAL_INTAKE_START, nearShootPose.y))
     val farIntake2Control = p(Pose(farStartPose.x, INTAKE_2_VERTICAL))
     val startIntake2   = p(Pose(HORIZONTAL_INTAKE_START, INTAKE_2_VERTICAL, INTAKE_HEADING))
