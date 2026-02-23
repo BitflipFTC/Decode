c4578fb30005ef31475875830c6bdda60a2d3065
UnheardWolf344 <griffin3868@gmail.com>
2026-02-22 21:05:47 -0500

    far cycling 12 ball

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RaptoriaAllianceAutonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RaptoriaAllianceAutonomous.kt
@@ -1,8 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import org.firstinspires.ftc.teamcode.opmodes.auto.paths.GarnetSquadron
-import org.firstinspires.ftc.teamcode.opmodes.teleop.CombinedTeleOp
+import org.firstinspires.ftc.teamcode.opmodes.auto.paths.RaptoriaAlliance
 import org.firstinspires.ftc.teamcode.opmodes.teleop.CombinedTeleOp.Companion.follower
 import org.firstinspires.ftc.teamcode.util.Alliance
 import org.firstinspires.ftc.teamcode.util.FiniteStateMachine
@@ -11,28 +10,38 @@ import org.firstinspires.ftc.teamcode.util.WaitState
 
 @Suppress("Unused")
 @Autonomous(name = "Garnet Squadron Partner", preselectTeleOp = "Combined TeleOp")
-class GarnetSquadronAutonomous: BaseAutonomous() {
+class RaptoriaAllianceAutonomous: BaseAutonomous() {
     override fun initialize(alliance: Alliance) {
-        pathSequence = GarnetSquadron(alliance)
+        pathSequence = RaptoriaAlliance(alliance)
         paths = pathSequence.buildPaths(follower!!)
         follower!!.setStartingPose(pathSequence.poses.farStartPose)
 
         finiteStateMachine = FiniteStateMachine(
             FollowPathState("Score preload", paths[0]),
             startIntake(),
-            WaitState(100.0),
             shootState(),
-            FollowPathState("D HP & Intake", paths[1]),
+
+            FollowPathState("D HP", paths[1]),
+            FollowPathState("Intake", paths[2]),
             WaitState(150.0),
-            FollowPathState("D Score", paths[2]),
+            FollowPathState("D Score", paths[3]),
             shootState(),
-            FollowPathState("dtointake", paths[3]),
-            WaitState(1500.0),
-            FollowPathState("do the intake", paths[4]),
-            WaitState(1000.0),
-            FollowPathState("d to shoito", paths[5]),
+
+            FollowPathState("dtointake", paths[4]),
+            WaitState(500.0),
+            FollowPathState("do the intake", paths[5]),
+            WaitState(500.0),
+            FollowPathState("d to shoito", paths[6]),
             shootState(),
-            FollowPathState("Park", paths[6])
+
+            FollowPathState("dtointake2", paths[7]),
+            WaitState(500.0),
+            FollowPathState("do the intake2", paths[8]),
+            WaitState(500.0),
+            FollowPathState("d to shoito2", paths[9]),
+            shootState(),
+
+            FollowPathState("Park", paths[10])
         )
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/paths/RaptoriaAlliance.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/paths/RaptoriaAlliance.kt
@@ -4,18 +4,13 @@ import com.pedropathing.follower.Follower
 import com.pedropathing.geometry.BezierLine
 import com.pedropathing.paths.PathChain
 import com.pedropathing.paths.callbacks.ParametricCallback
-import com.pedropathing.paths.callbacks.PoseCallback
-import com.pedropathing.paths.callbacks.TemporalCallback
-import org.firstinspires.ftc.teamcode.opmodes.auto.BaseAutonomous.Companion.PARAMETRIC_END
 import org.firstinspires.ftc.teamcode.util.Alliance
 import org.firstinspires.ftc.teamcode.util.auto.BaseAutoPath
 import org.firstinspires.ftc.teamcode.util.auto.Path
 import org.firstinspires.ftc.teamcode.util.buildBasicLine
-import org.firstinspires.ftc.teamcode.util.buildCurvedLine
 import org.firstinspires.ftc.teamcode.util.buildTangentLine
-import org.firstinspires.ftc.teamcode.util.doIntakeSpeed
 
-class GarnetSquadron(alliance: Alliance): BaseAutoPath(alliance) {
+class RaptoriaAlliance(alliance: Alliance): BaseAutoPath(alliance) {
 
     val fullSpeed = 0.8
     val intakeSpeed = 0.5
@@ -26,7 +21,10 @@ class GarnetSquadron(alliance: Alliance): BaseAutoPath(alliance) {
     lateinit var dScore: PathChain
     lateinit var dIntakeGate: PathChain
     lateinit var intakeGate: PathChain
-    lateinit var dScore2: PathChain
+    lateinit var dScoreGate: PathChain
+    lateinit var dIntakeGate2: PathChain
+    lateinit var intakeGate2: PathChain
+    lateinit var dScoreGate2: PathChain
     lateinit var park: PathChain
 
     override fun buildPaths(follower: Follower): List<Path> {
@@ -53,7 +51,18 @@ class GarnetSquadron(alliance: Alliance): BaseAutoPath(alliance) {
             .setLinearHeadingInterpolation(poses.HPPark.heading, Math.toRadians(330.0), 1.0, 0.0)
             .build()
 
-        dScore2 = follower.pathBuilder()
+        dScoreGate = follower.pathBuilder()
+            .buildTangentLine(poses.HPIntakeEnd, poses.farShootPose).setReversed().build()
+
+        dIntakeGate2 = follower.pathBuilder()
+            .buildTangentLine(poses.farShootPose, poses.HPPark).build()
+
+        intakeGate2 = follower.pathBuilder()
+            .addPath(BezierLine(poses.HPPark, poses.HPIntakeEnd))
+            .setLinearHeadingInterpolation(poses.HPPark.heading, Math.toRadians(330.0), 1.0, 0.0)
+            .build()
+
+        dScoreGate2 = follower.pathBuilder()
             .buildTangentLine(poses.HPIntakeEnd, poses.farShootPose).setReversed().build()
 
         park = follower.pathBuilder()
@@ -62,10 +71,14 @@ class GarnetSquadron(alliance: Alliance): BaseAutoPath(alliance) {
         return listOf(
             Path(scorePreload, fullSpeed),
             Path(dHP, intakeSpeed),
+            Path(HP, intakeSpeed),
             Path(dScore, fullSpeed),
             Path(dIntakeGate, fullSpeed),
             Path(intakeGate, intakeSpeed),
-            Path(dScore2, fullSpeed),
+            Path(dScoreGate, fullSpeed),
+            Path(dIntakeGate2, fullSpeed),
+            Path(intakeGate2, intakeSpeed),
+            Path(dScoreGate2, fullSpeed),
             Path(park, fullSpeed)
         )
     }
